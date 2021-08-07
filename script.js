1e1c43e6b243540f3667842f676acae23b0251e1
Tiffany Quon <tiffany.quon@alumni.ubc.ca>
2021-08-06 17:22:27 -0700

    remove console log statements

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function toggleReplayButton() {
   const replayButton = document.getElementById("replay");
-  console.log(replayButton);
-  console.log("here");
-  console.log(replayButton.style.visibility);
   if (replayButton.style.visibility === "visible") {
-    console.log("and now, here");
     replayButton.style.visibility = "hidden";
   } else {
     replayButton.style.visibility = "visible";
