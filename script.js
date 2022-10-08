6b872e6487fb2739b11313a443499ff376fdd0f7
Tiffany Quon <tiffany.quon@alumni.ubc.ca>
2022-10-08 15:52:29 -0700

    revert to sunrise

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ function toggleReplayButton() {
 
 window.onload = function() {
   const replayButton = document.getElementById("replay");
-  replayButton.addEventListener("click", resetSunset);
+  replayButton.addEventListener("click", resetSunrise);
 }
